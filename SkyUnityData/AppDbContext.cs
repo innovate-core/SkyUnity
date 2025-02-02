d8607822377ec0ce1c47e7392d266d24cb72c040
Innovate Core <kolaymaksimiv@gmail.com>
2025-02-02 15:07:28 +0200

    init user

--- SkyUnityData/AppDbContext.cs
+++ SkyUnityData/AppDbContext.cs
@@ -1,15 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SkyUnityCore.Entities;
 
 namespace SkyUnityCore;
 
 public class AppDbContext : DbContext
 {
+    DbSet<User> Users { get; set; }
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<User>().ToTable("users")
+            .HasKey(u => u.Id);
+
+        modelBuilder.Entity<User>()
+            .HasIndex(x => x.Name)
+            .IsUnique();
+
+        modelBuilder.Entity<User>()
+            .HasIndex(x => x.Email)
+            .IsUnique();
+
         base.OnModelCreating(modelBuilder);
     }
 }
--- SkyUnityData/Migrations/20250202130630_Init_User.Designer.cs
+++ SkyUnityData/Migrations/20250202130630_Init_User.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SkyUnityCore;
 
@@ -10,9 +11,11 @@ using SkyUnityCore;
 namespace SkyUnityCore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250202130630_Init_User")]
+    partial class Init_User
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
